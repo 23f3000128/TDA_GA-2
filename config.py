b156d38a2883b5119a6dee710fec7e745f3e0366
Don J Benoy <donjb003@gmail.com>
2026-07-04 13:41:16 +0530

    Setup config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+EMAIL= "23f3000128@ds.study.iitm.ac.in"
+Q1_ALLOWED_ORIGIN= "https://dash-tylzgy.example.com"
+ISSUER=	"https://idp.exam.local"
+AUDIENCE=	"tds-noglu4k4.apps.exam.local"
+PUBLIC_KEY_PEM=	
+"""-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----"""
+Q3_PORT=8457
+Q3_WORKERS=15
+Q3_DEBUG=True
+Q3_LOG_LEVEL="warning"
+Q5_API_KEY="ak_maj6toiiwmacf8x8lhmyzs7q"
+Q9_TOTAL_ORDERS=43
+Q9_RATE_LIMIT	Under=17 
+Q10_ALLOWED_ORIGIN="https://app-poq0ho.example.com"
+Q10_RATE_LIMIT=9
+
+EXAM_PORTAL_ORIGIN = "https://exam.sanand.workers.dev"
